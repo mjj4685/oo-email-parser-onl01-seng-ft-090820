3fcf6ae6e0cd19ef46a760ba81e2f3b147d1a9e2
MJ Johnson <mjohnson4685@gmail.com>
2020-09-21 01:03:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,8 +1,8 @@
-class EmailParser 
+class EmailParser
   attr_accessor :name, :csv_emails
-  
+
   def initialize(csv_emails)
 #   # you don't need name in here, it is redundant/
 #   @name = name
     @csv_emails = csv_emails
-  end 
+  end
